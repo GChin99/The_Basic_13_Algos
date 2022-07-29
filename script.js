8bb5570dec481404385e4366854b6d7ea5be009c
Gchin99 <gchin99@gmail.com>
2022-07-29 08:19:05 -0700

    completed 1-5

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ function count(){
         console.log(i);
     }
 }
-
 // count ()
 
-// 2. 
+// 2. Print all odd integers from 1 to 255
+function oddCount(){
+    for(var i=1; i<256; i+=2){
+        console.log(i)
+    }
+}
+// oddCount()
 
 // 3. Print ints and sum 0-255: Printe integers from 0 to 255 and with each integer print the sum so far 
 function intsAndSum(){
@@ -18,8 +23,16 @@ function intsAndSum(){
 
     }
 }
-
 // intsAndSum()
+
+// 4. Iterate and print Array: Iterate through a given arry, printing each value. 
+function newArr(arr){
+    for(var i=0; i<arr.length; i++){
+    // console.log(arr[i]);
+    }
+}
+var result = newArr([5, 10, 15, 20, 25, 30, 35]);
+
 
 // 5. Find and Print Max: Given an array, find and print its largest element
 var myArray = [6, 30, 4, 77, 56, 100, 88, 200]  //we just made up the array 
@@ -32,7 +45,6 @@ function largestElement(arr){
             largestValue=arr[i];
         }
     }
-    console.log(largestValue);
+    //console.log(largestValue);
 }
-
-largestElement(myArray)  //the return value doesnt work if the largest vaule is the last index of the array. 
+//largestElement(myArray)  //the return value doesnt work if the largest vaule is the last index of the array. 
